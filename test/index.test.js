37951c8f52438c57d58c6d19ce3b8a3b6f940083
邵振华 <s@zhso.net>
2016-08-30 08:51:41 +0800

    fix unit test case issue.

--- test/index.test.js
+++ test/index.test.js
@@ -2,65 +2,67 @@
 const exif = require("./../index.js");
 let expect = require("chai").expect;
 describe(".parse()", () => {
-    it("file {undefined}", ()=> {
-        expect(exif.parse).to.throw(Error);
+    it("file {undefined}", () => {
+        exif.parse(undefined, err => {
+            expect(err).to.throw(Error);
+        });
     });
-    it("file {null}", ()=> {
-        exif.parse("./test/null.jpg", err=> {
+    it("file {null}", () => {
+        exif.parse("./test/null.jpg", err => {
             expect(err).to.throw(Error);
         })
     });
-    it("APP1:#0xffe1", done=> {
+    it("APP1:#0xffe1", done => {
         exif.parse("./test/IMG_0001.JPG", (err, data) => {
             expect(data).to.be.an("object");
             done();
         });
     });
-    it("APP0:#0xffe0", done=> {
-        exif.parse("./test/IMG_0003.JPG", (err, data)=> {
+    it("APP0:#0xffe0", done => {
+        exif.parse("./test/IMG_0003.JPG", (err, data) => {
             expect(data).to.be.an("undefined");
             done();
         });
     });
-    it("!(APP1:#0xffe1||APP0:#0xffe0)", done=> {
-        exif.parse("./test/index.test.js", (err, data)=> {
+    it("!(APP1:#0xffe1||APP0:#0xffe0)", done => {
+        exif.parse("./test/index.test.js", (err, data) => {
             expect(data).to.be.an("undefined");
             done();
         });
     });
-    it("[SubExif]", done=> {
-        exif.parse("./test/IMG_0001.JPG", (err, data)=> {
+    it("[SubExif]", done => {
+        exif.parse("./test/IMG_0001.JPG", (err, data) => {
             expect(data.SubExif).to.be.an("object");
             done();
         });
     });
-    it("[GPSInfo]", done=> {
-        exif.parse("./test/IMG_0001.JPG", (err, data)=> {
+    it("[GPSInfo]", done => {
+        exif.parse("./test/IMG_0001.JPG", (err, data) => {
             expect(data.GPSInfo).to.be.an("object");
             done();
         });
     });
 });
-describe(".parseSync()", ()=> {
-    it("file {undefined}", ()=> {
+describe(".parseSync()", () => {
+    it("file {undefined}", () => {
         expect(exif.parseSync).to.throw(Error);
     });
-    it("file {null}", ()=> {
+    it("file {null}", () => {
         expect(exif.parseSync).to.throw(Error);
     });
-    it("APP1:#0xffe1", ()=> {
+    it("APP1:#0xffe1", () => {
         let data = exif.parseSync("./test/IMG_0001.JPG");
         expect(data).to.be.an("object");
     });
-    it("!APP1:#0xffe1", ()=> {
+    it("!APP1:#0xffe1", () => {
         let data = exif.parseSync("./test/IMG_0003.JPG");
         expect(data).to.be.an("object");
     });
-    it("[SubExif]", ()=> {
+    it("[SubExif]", () => {
         let data = exif.parseSync("./test/IMG_0001.JPG");
         expect(data.SubExif).to.be.an("object");
     });
-    it("[GPSInfo]", ()=> {
+    it("[GPSInfo]", () => {
         let data = exif.parseSync("./test/IMG_0001.JPG");
         expect(data.GPSInfo).to.be.an("object");
     });
